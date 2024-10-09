959ea256b2648427bd78689fa1607707659aa5c6
agilematrix <8009878+bikashmoktan@users.noreply.github.com>
2024-10-09 17:18:09 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,16 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this for you .</p>
+    <p>Do not ask theoretical questions. Theories solve less and confuse more. If there were no theories, there would be less problems. It is not that theories solve questions or problems. On the contrary, questions arise out of theories.
+
+And do not ask philosophical questions. Philosophical questions only seem to be questions, but they are not. That is why no answer has been possible. If a question is really a question then it is answerable, but if it is false, just a linguistic confusion, then it cannot be answered. Philosophy has gone on answering for centuries and centuries, but the questions still remain the same. However you answer a philosophical question you never answer it, because the question itself is false. It is not meant to be answered at all. The question is such that, intrinsically, no answer is possible.
+
+And do not ask metaphysical questions. For example, if you ask who created the world, it is unanswerable. It is absurd. It is not that metaphysical questions are not real questions, but they cannot be answered. They can be solved, but they cannot be answered.
+
+Ask questions that are personal, intimate, existential. One must be aware of what one is really asking. Is it something that really means something to you? If it is answered, will a new dimension open for you? Will something be added to your existence, will your being in any way be transformed through it? Only such questions are religious.
+
+Religion is concerned with problems, not with questions. A question may just come out of curiosity, but a problem is intimate and personal. You are involved in it; it is you. A question is separate from you; a problem is you. So before asking anything, dig deep inside and ask something that is intimate and personal, something in which you are confused, in which you are involved. Only then can you be helped.
+    </p>
     </Layout>
   )
 }
