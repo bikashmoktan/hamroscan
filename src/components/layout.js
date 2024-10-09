c7d4f705801b52b4606e5afba79f3a2219b50ba6
agilematrix <agilematrix@users.noreply.github.com>
2024-10-09 18:13:47 -0400

    h
    h

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,36 +1,45 @@
-import * as React from 'react'
-import { Link } from 'gatsby'
+import * as React from 'react';
+import { Link } from 'gatsby';
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
-  navLinkText
-} from './layout.module.css'
+  navLinkText,
+  header,
+  footer,
+  mainContent,
+} from './layout.module.css';
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <div className={container}>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
+      <header className={header}>
+        <h1>My Awesome Website</h1>
+        <nav>
+          <ul className={navLinks}>
+            <li className={navLinkItem}>
+              <Link to="/" className={navLinkText}>
+                Home
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/about" className={navLinkText}>
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className={mainContent}>
         <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
+      <footer className={footer}>
+        <p>© {new Date().getFullYear()} My Awesome Website. All rights reserved.</p>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
